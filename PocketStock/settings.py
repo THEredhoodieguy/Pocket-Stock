16dd59831be9f51b8bbec305e1a13675457999a8
Matthew Pletcher <sonicfan321@gmail.com>
2017-10-04 17:10:31 -0400

    changed static files base location

--- PocketStock/settings.py
+++ PocketStock/settings.py
@@ -140,7 +140,7 @@ DUO_HOST = 'api-ccfb0134.duosecurity.com'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, 'stocks/static/')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'stocks/static/'),
 ]
