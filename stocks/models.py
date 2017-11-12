bb5fffcbab657098a67277a62a951d741fd08cf8
Matthew Pletcher <sonicfan321@gmail.com>
2017-11-12 12:35:03 -0500

    added a string for admin-side viewing of chat rooms

--- stocks/models.py
+++ stocks/models.py
@@ -88,6 +88,9 @@ class Room(models.Model):
     name = models.TextField()
     label = models.SlugField(unique=True)
 
+    def __unicode__(self):
+        return str(self.label)
+
 class Message(models.Model):
     room = models.ForeignKey(Room, related_name='messages')
     handle = models.TextField()
