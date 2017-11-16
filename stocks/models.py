b83df522fde92dddbf080ca2047f859136f39f7d
Matthew Pletcher <sonicfan321@gmail.com>
2017-11-16 12:32:47 -0500

    added methods for message class

--- stocks/models.py
+++ stocks/models.py
@@ -96,3 +96,13 @@ class Message(models.Model):
     handle = models.TextField()
     message = models.TextField()
     timestamp = models.DateTimeField(default=timezone.now, db_index=True)
+
+    def __unicode__(self):
+        return '[{timestamp}] {handle}: {message}'.format(**self.as_dict())
+
+    @property
+    def formatted_timestamp(self):
+        return self.timestamp.strftime('%b %-d %-I:%M %p')
+    
+    def as_dict(self):
+        return {'handle': self.handle, 'message': self.message, 'timestamp': self.formatted_timestamp}
